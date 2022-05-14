8a32eb2287c5eefaf60786e7a6902869c66245a8
Jefferson Penna <penna.jefferson@gmail.com>
2022-05-14 12:17:28 -0300

    chore: add and configure eslint

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,6 @@ import express from 'express'
 
 const app = express()
 
-app.get('/', (request, response) => {
-    return response.json( { siteName: "TODO List" } )
-})
+app.get('/', (request, response) => response.json({ siteName: 'TODO List' }))
 
 app.listen(3333)
