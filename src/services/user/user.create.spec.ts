02fbac7f235e7d6847734e08ac0e86d289858177
Jefferson Penna <penna.jefferson@gmail.com>
2022-05-15 15:56:34 -0300

    test: validate create user

---
+++ src/services/user/user.create.spec.ts
@@ -0,0 +1,46 @@
+import { User } from '@models/User'
+import { UsersRepositoryInMemory } from '@repositories/in-memory/InMemoryUsersRepository'
+import { IUsersRepository } from '@repositories/IUsersRepositories'
+import { UserCreateService } from './user.create'
+
+import Messages from '@constants/messages'
+import AppError from '@config/appError'
+
+describe('Create user', () => {
+  let usersRepository: IUsersRepository
+  let userCreateService: UserCreateService
+
+  beforeAll(() => {
+    usersRepository = new UsersRepositoryInMemory()
+    userCreateService = new UserCreateService(usersRepository)
+  })
+
+  it('should be able to create a new user', async () => {
+    const userData: User = {
+      firstName: 'Jefferson',
+      lastName: 'Penna',
+      email: 'penna.jefferson@gmail.com',
+      password: 'Senha1234'
+    }
+
+    const user = await userCreateService.exec(userData)
+
+    expect(user).toHaveProperty('id')
+    expect(user.email).toBe('penna.jefferson@gmail.com')
+  })
+
+  it('should not be able to create an existing user', async () => {
+    const userData: User = {
+      firstName: 'Jefferson',
+      lastName: 'Penna',
+      email: 'penna.jefferson@gmail.com',
+      password: 'Senha1234'
+    }
+
+    await userCreateService.exec(userData)
+
+    await expect(userCreateService.exec(userData)).rejects.toEqual(
+      new AppError(Messages.USER_VALIDATE_DUPLICATED_EMAIL, 400)
+    )
+  })
+})
